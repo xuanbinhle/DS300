115d9a8ce6e26760bdba3790bfd7ea7df3bb7b84
Lam <thaiminhlam1177@gmail.com>
2026-01-03 18:51:57 +0700

    update: erase tempfile

--- b.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-products = pd.read_csv("./data/features/final_cleaned_books.csv")
-reviews = pd.read_csv("./data/features/final_interactions.csv")
-print(products.shape, reviews.shape)
-
-
-
